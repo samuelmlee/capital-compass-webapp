7dff4e0f9ed5ce451d63ca93660ed1eb60197ccb
Samuel Lee <sam.leefr@gmail.com>
2023-11-13 20:02:55 +0100

    no proxy

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -9,19 +9,21 @@ import { User } from '../users/user';
 export class AuthService {
     constructor(private httpClient: HttpClient) {}
 
+    private apiUrl = 'http://localhost:8082';
+
     authenticate(): Observable<User> {
-        return this.httpClient.get<User>(`/user`, {
+        return this.httpClient.get<User>(`${this.apiUrl}/user`, {
             withCredentials: true,
         });
     }
 
     login(): void {
-        window.open(`/oauth2/authorization/keycloak`, '_self');
+        window.open(`${this.apiUrl}/oauth2/authorization/keycloak`, '_self');
     }
 
     logout(): Observable<any> {
         return this.httpClient.post<any>(
-            `/logout`,
+            `${this.apiUrl}/logout`,
             {},
             {
                 withCredentials: true,
