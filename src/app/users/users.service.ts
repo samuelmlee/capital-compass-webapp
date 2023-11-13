9c624f8e94886f66fa26566a64aaccd2c2f06fed
Samuel Lee <sam.leefr@gmail.com>
2023-11-13 19:25:45 +0100

    cleanup

--- src/app/users/users.service.ts
+++ src/app/users/users.service.ts
@@ -1,17 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { environment } from '../../environments/environment';
 
 @Injectable({
     providedIn: 'root',
 })
 export class UsersServiceService {
-    private apiUrl = environment.apiUrl;
-
     constructor(private http: HttpClient) {}
 
     getUserProfile(): Observable<any> {
-        return this.http.get(`${this.apiUrl}/`);
+        return this.http.get(`/users`);
     }
 }
