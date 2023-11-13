00298d1c2151de730b116299fcd0f8fd42ecbf81
Samuel Lee <sam.leefr@gmail.com>
2023-11-13 20:14:12 +0100

    clean up

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,5 @@
 import { Component } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
 import { AuthService } from './auth/auth.service';
-import { User } from './users/user';
 
 @Component({
     selector: 'app-root',
@@ -9,30 +7,22 @@ import { User } from './users/user';
     styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-    title = 'Capital Compass';
+    public title = 'Capital Compass';
 
-    isAuthenticated$ = new BehaviorSubject<boolean>(false);
-    user$ = new BehaviorSubject<User | undefined>(undefined);
+    public isAuthenticated$ = this.authService.isAuthenticated$;
+    public user$ = this.authService.user$;
 
     constructor(private authService: AuthService) {}
 
     ngOnInit(): void {
-        this.authService.authenticate().subscribe((user) => {
-            if (user) {
-                this.isAuthenticated$.next(true);
-                this.user$.next(user);
-            }
-        });
+        this.authService.initAuthentication();
     }
 
-    logInClicked(): void {
+    public logInClicked(): void {
         this.authService.login();
     }
 
-    logOutClicked() {
-        this.authService.logout().subscribe(() => {
-            this.isAuthenticated$.next(false);
-            this.user$.next(undefined);
-        });
+    public logOutClicked() {
+        this.authService.logout();
     }
 }
--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,15 +1,27 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { BehaviorSubject, Observable } from 'rxjs';
 import { User } from '../users/user';
 
 @Injectable({
     providedIn: 'root',
 })
 export class AuthService {
+    public isAuthenticated$ = new BehaviorSubject<boolean>(false);
+    public user$ = new BehaviorSubject<User | undefined>(undefined);
+
     constructor(private httpClient: HttpClient) {}
 
     private apiUrl = 'http://localhost:8082';
+
+    initAuthentication() {
+        this.authenticate().subscribe((user) => {
+            if (user) {
+                this.isAuthenticated$.next(true);
+                this.user$.next(user);
+            }
+        });
+    }
 
     authenticate(): Observable<User> {
         return this.httpClient.get<User>(`${this.apiUrl}/user`, {
@@ -21,13 +33,18 @@ export class AuthService {
         window.open(`${this.apiUrl}/oauth2/authorization/keycloak`, '_self');
     }
 
-    logout(): Observable<any> {
-        return this.httpClient.post<any>(
-            `${this.apiUrl}/logout`,
-            {},
-            {
-                withCredentials: true,
-            }
-        );
+    public logout(): void {
+        this.httpClient
+            .post<any>(
+                `${this.apiUrl}/logout`,
+                {},
+                {
+                    withCredentials: true,
+                }
+            )
+            .subscribe(() => {
+                this.isAuthenticated$.next(false);
+                this.user$.next(undefined);
+            });
     }
 }
