a01dfbb65d43ec47b99efefd749f89e0cfad4ae5
Samuel Lee <sam.leefr@gmail.com>
2023-11-24 13:48:36 +0100

    unsubscribe

--- src/app/tickers/service/tickers.service.ts
+++ src/app/tickers/service/tickers.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpParams } from '@angular/common/http'
-import { Injectable, Signal, signal } from '@angular/core'
-import { catchError, map, of } from 'rxjs'
+import { Injectable, OnDestroy, Signal, signal } from '@angular/core'
+import { Subscription, catchError, map, of } from 'rxjs'
 import { environment } from 'src/environments/environment'
 import { TickersResponse, TickersResponseResult, TickersResponseSource } from '../model/tickers-response'
 import { TickersSearchConfig } from '../model/tickers-search-config'
@@ -8,28 +8,34 @@ import { TickersSearchConfig } from '../model/tickers-search-config'
 @Injectable({
   providedIn: 'root'
 })
-export class TickerService {
+export class TickerService implements OnDestroy {
   private tickersResponseSignal = signal<TickersResponseResult>({
     value: { results: [], nextCursor: '', source: null },
     error: null
   })
 
   private apiUrl = environment.apiUrl
+  private tickersSubsription: Subscription | undefined
 
   public constructor(private http: HttpClient) {}
+
+  public ngOnDestroy(): void {
+    this.tickersSubsription?.unsubscribe()
+  }
 
   public get tickersResponse(): Signal<TickersResponseResult> {
     return this.tickersResponseSignal.asReadonly()
   }
 
   public getTickersByConfig(searchConfig: TickersSearchConfig): void {
+    this.tickersSubsription?.unsubscribe()
     const options = {
       params: new HttpParams()
         .set('ticker', searchConfig?.ticker ?? '')
         .set('search-term', searchConfig?.searchTerm ?? '')
     }
 
-    this.http
+    this.tickersSubsription = this.http
       .get<TickersResponse>(`${this.apiUrl}/stocks/tickers`, options)
       .pipe(
         map(
@@ -41,7 +47,9 @@ export class TickerService {
   }
 
   public getTickersByCursor(cursor: string): void {
-    this.http
+    this.tickersSubsription?.unsubscribe()
+
+    this.tickersSubsription = this.http
       .get<TickersResponse>(`${this.apiUrl}/stocks/tickers/${cursor}`)
       .pipe(
         map(
