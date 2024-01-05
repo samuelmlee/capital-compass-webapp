cc343067216b537decac413e718f5f6bf16bce3b
Samuel Lee <sam.leefr@gmail.com>
2024-01-05 16:58:42 +0100

    ssl

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8082/v1',
-  authUrl: 'http://localhost:8082',
+  apiUrl: 'https://localhost:8443/v1',
+  authUrl: 'https://localhost:8443',
   gatewayClientId: 'capital-compass-gateway-client'
 }
