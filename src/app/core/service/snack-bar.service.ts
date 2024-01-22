54ff32fe98e3c151fb3975b8637c9480001bade5
Samuel Lee <sam.leefr@gmail.com>
2024-01-22 12:21:19 +0100

    snack back css

--- src/app/core/service/snack-bar.service.ts
+++ src/app/core/service/snack-bar.service.ts
@@ -8,14 +8,23 @@ export class SnackbarService {
   constructor(private _snackBar: MatSnackBar) {}
 
   public error(message: string): MatSnackBarRef<TextOnlySnackBar> {
-    return this._snackBar.open(message, undefined, { panelClass: ['snackbar-error'] })
+    return this._snackBar.open(message, 'Close', {
+      duration: 10000,
+      panelClass: ['error-snackbar']
+    })
   }
 
   public success(message: string): MatSnackBarRef<TextOnlySnackBar> {
-    return this._snackBar.open(message, undefined, { panelClass: ['snackbar-success'] })
+    return this._snackBar.open(message, 'Close', {
+      duration: 10000,
+      panelClass: ['success-snackbar']
+    })
   }
 
   public info(message: string): MatSnackBarRef<TextOnlySnackBar> {
-    return this._snackBar.open(message, undefined, { panelClass: ['snackbar-info'] })
+    return this._snackBar.open(message, 'Close', {
+      duration: 10000,
+      panelClass: ['info-snackbar']
+    })
   }
 }
