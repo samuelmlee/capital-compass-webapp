7e774e078d24b3cd0d9fad7b77de2e90bda5264e
Samuel Lee <sam.leefr@gmail.com>
2024-01-24 12:25:14 +0100

    tsconfig.spec

--- src/app/shared/utils/from-obs-to-signal.spec.ts
+++ src/app/shared/utils/from-obs-to-signal.spec.ts
@@ -1,4 +1,5 @@
 import { HttpErrorResponse } from '@angular/common/http'
+import { TestBed } from '@angular/core/testing'
 import { of } from 'rxjs'
 import { Result } from '../model/result'
 import { fromObsToSignal } from './from-obs-to-signal'
@@ -6,14 +7,14 @@ import { fromObsToSignal } from './from-obs-to-signal'
 describe('fromObsToSignal', () => {
   it('should accept an Observable as input and return a Result', () => {
     TestBed.runInInjectionContext(() => {
-      const obsResponse = of('Ressponse')
+      const obsResponse = of('Response')
       const processor = (e: HttpErrorResponse): string => {
         return 'Eror Processed'
       }
 
       const result: Result<string> = fromObsToSignal<string>(obsResponse, processor)
 
-      expect(result).toBeUndefined()
+      expect(result).toBeDefined()
     })
   })
 })
